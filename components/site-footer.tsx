7d85fc001aa9dbdff6e87c39d9cf629000b62e61
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-11-18 20:42:58 +0530

    feat: Modify site-footer file and site config Remove address and extra css from footer Remove address from site-config

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,26 +4,14 @@ import mitblr_club from "../public/mitblr_club.png";
 
 const SiteFooter = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-4 justify-around top-0 z-50  mx-6 lg:mx-20 border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        
-        <div className="flex flex-col gap-3 p-2">
-
+    <div className="flex flex-col lg:flex-row gap-2  lg:gap-4  items-center justify-around top-0 z-50  mx-6 lg:mx-20 border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 p-1 lg:p-3">
+              
           <div
           className="font-bold text-2xl"
           >
             mitblr.<span className="text-violet-500">club</span>
           </div>         
 
-          {/*<div className="flex flex-col gap-2">
-            <div className="font-semibold">Contact Us</div>
-
-            <div className="flex flex-col gap-1">
-              <div>Contact.No:{siteConfig.contactDetails.number}</div>
-              <div>EmailID: {siteConfig.contactDetails.email}</div>
-            </div>
-          </div>*/}
-
-        </div>
 
         <div className="flex items-center">
 
@@ -33,8 +21,6 @@ const SiteFooter = () => {
             className="hover:text-blue-600 hover:underline text-sm font-normal"
             >{siteConfig.institution}</a>
           </h2>
-
-          {/*<div className="text-sm">{siteConfig.address}</div>*/}
 
         </div>
   
