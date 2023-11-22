fe5affb69c9451f8d20bfdc60f9477524e5e66b7
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-11-22 22:50:41 +0530

    chore: formatted codebase using prettier

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,29 +1,26 @@
-import { siteConfig } from "@/site.config";
-import Image from "next/image";
-import mitblr_club from "../public/mitblr_club.png";
+import { siteConfig } from '@/site.config';
+
+import Image from 'next/image';
+
+import mitblr_club from '../public/mitblr_club.png';
 
 const SiteFooter = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-2  lg:gap-4  items-center justify-around top-0 z-50  mx-6 lg:mx-20 border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 p-1 lg:p-3">
-              
-          <div
-          className="font-bold text-2xl"
+    <div className="top-0 z-50 mx-6 flex  flex-col  items-center justify-around gap-2 border-t  bg-background/95 p-1 backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:mx-20 lg:flex-row lg:gap-4 lg:p-3">
+      <div className="text-2xl font-bold">
+        mitblr.<span className="text-violet-500">club</span>
+      </div>
+
+      <div className="flex items-center">
+        <h2>
+          <a
+            href={siteConfig.mediaLinks.insitute}
+            className="text-sm font-normal hover:text-blue-600 hover:underline"
           >
-            mitblr.<span className="text-violet-500">club</span>
-          </div>         
-
-
-        <div className="flex items-center">
-
-          <h2>
-            <a 
-            href={siteConfig.mediaLinks.insitute} 
-            className="hover:text-blue-600 hover:underline text-sm font-normal"
-            >{siteConfig.institution}</a>
-          </h2>
-
-        </div>
-  
+            {siteConfig.institution}
+          </a>
+        </h2>
+      </div>
     </div>
   );
 };
