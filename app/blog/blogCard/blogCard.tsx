ed29700f39216375fab5ca393c94c93f63cb37c5
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-12-17 19:04:29 +0530

    fix: changed design elements of card title
    also added an animation on hover

--- app/blog/blogCard/blogCard.tsx
+++ app/blog/blogCard/blogCard.tsx
@@ -1,74 +1,76 @@
-import data from './data';
-
-import { 
-Card,
-CardHeader,
-CardTitle,
-CardContent,
-CardFooter} from '@/components/ui/card';
-
 import Link from 'next/link';
 
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import data from './data';
 
 export default function BlogCard() {
+  const descLength: number = 100;
+  const titleLength: number = 50;
 
-    const descLength:number = 100
-    const titleLength:number = 40
+  return (
+    <div className="mt-c15 md:mt-c5 xl:mt-c3 ">
+      <div className="flex flex-col gap-3 md:grid md:grid-cols-2 xl:grid-cols-3">
+        {data.map((item: any, index: number) => {
+          let hiddenDescText: string = '';
 
-    return(
+          if (item.description.length > descLength) {
+            item.description =
+              item.description.substring(0, descLength - 3) + '...';
+          } else {
+            hiddenDescText = '* '.repeat(
+              (descLength - item.description.length) / 2
+            );
+          }
 
-        <div className="mt-c15 md:mt-c5 xl:mt-c3 ">
-            
-        <div className='flex flex-col md:grid md:grid-cols-2 xl:grid-cols-3 gap-3'>
+          let hiddenTitleText: string = '';
 
-            {data.map( (item:any,index:number) => {
-                
-                let hiddenDescText:string = ''
+          if (item.title.length > titleLength) {
+            item.title = item.title.substring(0, titleLength - 3) + '...';
+          } else {
+            hiddenTitleText = '* '.repeat(
+              (titleLength - item.title.length) / 2
+            );
+          }
 
-                if(item.description.length > descLength){
-                    item.description = item.description.substring(0,descLength-3) + '...'
-                }
-                else{
-                    hiddenDescText = '* '.repeat((descLength - item.description.length)/2)
-                }
-
-                let hiddenTitleText:string = ''
-
-                if(item.title.length > titleLength){
-                    item.title = item.title.substring(0,titleLength-3) + '...'
-                }
-                else{
-                    hiddenTitleText = '* '.repeat((titleLength - item.title.length)/2)
-                }
-
-                return(
-
-                    <div key={index} className="mt-c5">
-                        <Link href="/">
-                        <Card className="flex flex-col w-c80 md:w-c40 xl:w-c25 h-fit hover:border-primary focus:border-primary">
-                            <CardHeader>
-                                <div className="text-md font-light dark:font-extralight">{item.date}</div>
-                                <CardTitle>
-                                    <div className="text-2xl font-bold">{item.title}<span className='invisible'>{hiddenTitleText}</span></div>
-                                </CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <div className="text-sm font-medium dark:font-light h-fit overflow-clip">{item.description} <span className='invisible'>{hiddenDescText}</span></div>
-                            </CardContent>
-                            <CardFooter className='flex flex-col gap-3'>
-                                <div className="text-md font-medium dark:font-light">{item.author}</div>
-                            </CardFooter>
-                        </Card>
-                        </Link>
+          return (
+            <div key={index} className="mt-c5">
+              <Link href="/">
+                <Card className="flex h-fit w-c80 flex-col duration-300 ease-in-out hover:scale-105 hover:border-primary hover:shadow-lg focus:border-primary md:w-c40 xl:w-c25">
+                  <CardHeader>
+                    <div className="text-md font-light dark:font-extralight">
+                      {item.date}
                     </div>
-                )
-
-            })}
-
-        </div>
-
-        </div>
-
-    )
-
+                    <CardTitle className="flex flex-col justify-center">
+                      <div className="flex text-2xl font-bold">
+                        {item.title}
+                        <span className="invisible">{hiddenTitleText}</span>
+                      </div>
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="h-fit overflow-clip text-sm font-medium dark:font-light">
+                      {item.description}{' '}
+                      <span className="invisible">{hiddenDescText}</span>
+                    </div>
+                  </CardContent>
+                  <CardFooter className="flex flex-col gap-3">
+                    <div className="text-md font-medium dark:font-light">
+                      {item.author}
+                    </div>
+                  </CardFooter>
+                </Card>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,11 @@
-import BlogCard from "./blogCard/blogCard";
+import BlogCard from './blogCard/blogCard';
 
 const Blog = () => {
   return (
-    <div className="mt-c20 md:mt-c10 xl:mt-c5 px-0 xl:px-c8 pb-c3">
+    <div className="mt-c20 px-0 pb-c3 md:mt-c10 xl:mt-c5 xl:px-c8">
+      <div className="text-4xl font-bold">Latest Updates</div>
 
-        <div className="text-4xl font-bold">Latest Updates</div>
-
-        <BlogCard />
+      <BlogCard />
     </div>
   );
 };
